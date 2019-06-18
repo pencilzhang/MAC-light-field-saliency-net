922ba6985a9fa5872851e4a4fc7b7aff8e516a02
SArMg <1506685230@qq.com>
2019-06-19 00:10:52 +0800

    Update train_val_LFnet.py

--- models/LFnet_33/train_val_LFnet.py
+++ models/LFnet_33/train_val_LFnet.py
@@ -148,10 +148,10 @@ def cn_LF(split):
     return n.to_proto()
 
 def make_net():
-    with open('../LFNet_model/LFnet_33/train_LF.prototxt', 'w') as f:
+    with open('../models/LFnet_33/train_LF.prototxt', 'w') as f:
         f.write(str(cn_LF('train')))
 
-    with open('../LFNet_model/LFnet_33/val_LF.prototxt', 'w') as f:
+    with open('../models/LFnet_33/val_LF.prototxt', 'w') as f:
         f.write(str(cn_LF('val')))
 
 if __name__ == '__main__':
