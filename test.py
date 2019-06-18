6d1eab29986926e39dc0279d8b6cae09bc45ad8e
SArMg <1506685230@qq.com>
2019-06-19 00:27:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -19,9 +19,9 @@ caffe.set_mode_gpu()
 LFNet_model='LFnet_99'
 k=1
 
-caffemodel='../LFNet_model/'+LFNet_model+'model/'+str(k)+'train_iter_160000.caffemodel'
+caffemodel='../models/'+LFNet_model+'model/'+str(k)+'train_iter_160000.caffemodel'
 
-net_test = caffe.Net('../LFNet_model/'+LFNet_model+'/test.prototxt',  
+net_test = caffe.Net('../models/'+LFNet_model+'/test.prototxt',  
                 caffemodel,  
                 caffe.TEST) 
 test_iter =  128   
