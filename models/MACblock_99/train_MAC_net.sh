9378d82cbe1171846a31d90fbd593752e32e93e2
SArMg <1506685230@qq.com>
2019-10-08 14:16:56 +0800

    Create train_MAC_net.sh

---
+++ models/MACblock_99/train_MAC_net.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+set -e
+
+GLOG_logtostderr=0 GLOG_log_dir='/home/dhubel/deeplab_v2/LFNet/LFNet_model/MACblock_99/log/' ./build/tools/caffe train --solver=/home/dhubel/deeplab_v2/LFNet/LFNet_model/MACblock_99/solver_train_LF_aug.prototxt --weights=/home/dhubel/deeplab_v2/pretrain/pretrain.caffemodel --gpu=0$@
